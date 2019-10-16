f0cf982114a4c3850f5b9339d544c66dd09bef7c
shotaro yuki <yuki8118tmds@gmail.com>
2019-10-16 11:52:27 +0900

    タイムゾーンを日本（東京）に設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Manyou
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.i18n.default_locale = :ja
     config.load_defaults 5.2
