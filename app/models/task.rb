fa643869108e9ffa1288e6be0ca73634ab6713e3
shotaro yuki <yuki8118tmds@gmail.com>
2019-10-22 18:06:31 +0900

    re

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true, length: { maximum: 255 }
+  scope :title_scope, ->(name) { where('title LIKE ?', "%#{name}%") }
+  scope :status_scope, ->(state) { where('status LIKE ?', "%#{state}%") }
+  enum priority: { 低: 0, 中: 1, 高: 2 }
 end
